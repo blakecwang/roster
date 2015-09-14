c0a5dd84bdfcf0f60ecf3f0acdd214881929fe75
Blake Wang <blakecwang@gmail.com>
2015-09-13 20:39:59 -0700

    randomly assign genders

--- js/test_data.js
+++ js/test_data.js
@@ -1,4 +1,4 @@
-var testData = []	
+var testData = [];
 
 function initTestData() {
 
@@ -8,51 +8,54 @@ function initTestData() {
 
 
 	// set ratios  for different perameters
-	var ratios = [
-		0.5,	// 0	males
-		0.1,	// 1 	reading - high
-		0.5,	// 2 	reading - mid
-		0.2,	// 3 	writing - high
-		0.2,	// 4 	writing - mid
-		0.2,	// 5 	math - high
-		0.6,	// 6 	math - mid
-		0.1,	// 7 	red dot
-		0.1,	// 8 	IEP
-		0.1,	// 9 	health concerns
-		0.4		// 10 	attended TK
-	];
+	var ratios = {
+		"males": 0.5,
+		"readingHigh": 0.1,
+		"readingMid": 0.5,
+		"writingHigh": 0.2,
+		"writingMid": 0.2,
+		"mathHigh": 0.2,
+		"mathMid": 0.6,
+		"redDot": 0.1,
+		"iep": 0.1,
+		"health": 0.1,
+		"tk": 0.4
+	};
+
+	// ratios.females = 1 - ratios.males;
+	// ratios.readingLow = 1 - ratios.readingHigh - ratios.readingMid;
+	// ratios.writingLow = 1 - ratios.writingHigh - ratios.writingMid;
+	// ratios.mathLow = 1 - ratios.mathHigh - ratios.mathMid;
 
 
 	// calculate actual numbers of students with each parameter
-	var actualNumbers = [];
-	for (var j = 0; j < ratios.length; j++) {
+	var actualNumbers = ratios;
+	for (var j in actualNumbers) {
 
-		var n = Math.floor(ratios[j] * students);
-		actualNumbers.push(n);
+		var n = Math.round(actualNumbers[j] * students);
+		actualNumbers[j] = n;
 
 	}
 		
 
-	// define template object to create student objects
-	var testDataTemplate = {
-
-		"name": undefined,
-		"gender": undefined,
-		"reading": undefined,
-		"writing": undefined,
-		"math": undefined,
-		"reddot": undefined,
-		"iep": undefined,
-		"health": undefined,
-		"tk": undefined,
-		"teacher": undefined,
-		"separate": undefined
-		
-	};
-
-
 	// add template objects to testData
 	for (var i = 0; i < students; i++) {
+
+		var testDataTemplate = {
+
+			"name": undefined,
+			"gender": undefined,
+			"reading": undefined,
+			"writing": undefined,
+			"math": undefined,
+			"reddot": undefined,
+			"iep": undefined,
+			"health": undefined,
+			"tk": undefined,
+			"teacher": undefined,
+			"separate": undefined
+			
+		};
 
 		testData.push(testDataTemplate);
 
@@ -92,11 +95,50 @@ function initTestData() {
 	}
 
 	
-	var s = makeSubset([1,2,3,4,5,6,7,8,9], 3);
-	console.log(s);
+	function removeSubset(superset, subset) {
+
+		var newSet = [];
+		for (var i = 0; i < superset.length; i++) {
+
+			for (var j = 0; j < subset.length; j++) {
+
+				if (superset[i] != subset[j]) {
+					newSet.push(superset[i]);
+				}
+
+			}
+
+		}
+
+		return newSet;
+
+	}
+
+	// assign male and female randomly
+	var testDataIndices = [];
+	for (var k = 0; k < testData.length; k++) {
+		testDataIndices.push(k);
+	}
+	var s = makeSubset(testDataIndices, actualNumbers.males);
+
+	for (var j = 0; j < s.length; j++) {
+		testData[s[j]].gender = "male";
+	}
+	testData[3].gender = "male";
+
+	for (var k = 0; k < testData.length; k++) {
+		if (testData[k].gender != "male") {
+			testData[k].gender = "female";
+		}
+	}
 
 
 
+	var genderArr = [];
+	for (var p = 0; p < testData.length; p++) {
+		genderArr.push(testData[p].gender);
+	}
+	console.log(genderArr);
 
 	
 
